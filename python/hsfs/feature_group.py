ee4e97b44dba30b602cf323736a718140932638f
davitbzh <davit@logicalclocks.com>
2021-01-08 00:24:01 +0100

    move compute stat to fgbase

--- python/hsfs/feature_group.py
+++ python/hsfs/feature_group.py
@@ -230,7 +230,10 @@ class FeatureGroupBase:
             `RestAPIError`. Unable to persist the statistics.
         """
         if self.statistics_config.enabled:
-            return self._statistics_engine.compute_statistics(self, self.read())
+            commit_date = self._get_latest_commit_date()
+            return self._statistics_engine.compute_statistics(
+                self, self.read(), commit_date
+            )
         else:
             warnings.warn(
                 (
