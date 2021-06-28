6e24f733eb35cd5e17eb0b70031d396c1b726d3e
davitbzh <davit@logicalclocks.com>
2021-06-28 17:36:25 +0200

    licence

--- java/src/main/java/com/logicalclocks/hsfs/metadata/SplitStatistics.java
+++ java/src/main/java/com/logicalclocks/hsfs/metadata/SplitStatistics.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-
 package com.logicalclocks.hsfs.metadata;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
