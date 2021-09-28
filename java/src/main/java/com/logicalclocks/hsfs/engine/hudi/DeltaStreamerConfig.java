ea071fdbe97ee895bbebece76ef8ff2e04de9571
davitbzh <davit@logicalclocks.com>
2021-09-28 11:14:19 +0200

    bluh

--- java/src/main/java/com/logicalclocks/hsfs/engine/hudi/DeltaStreamerConfig.java
+++ java/src/main/java/com/logicalclocks/hsfs/engine/hudi/DeltaStreamerConfig.java
@@ -96,6 +96,5 @@ public class DeltaStreamerConfig implements Serializable {
     System.out.println("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
     deltaSync.getDeltaSyncService().getDeltaSync().getProps().get("ValidationId");
     System.out.println("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
-    deltaSync.getDeltaSyncService().waitForShutdown();
   }
 }
